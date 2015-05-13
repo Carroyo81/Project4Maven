8ad5d17cc85c62acc79c4e668234c6f6fe435b48
Carroyo81 <charlie.a1011@gmail.com>
2015-05-13 11:49:17 -0500

    put

--- src/main/java/com/mycompany/project3maven/MyResource.java
+++ src/main/java/com/mycompany/project3maven/MyResource.java
@@ -13,6 +13,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
@@ -68,11 +69,10 @@ public class MyResource {
 
         return cm.getCust();
     }
-
-    @Path("upCust")
+@Path("upCust/{inputUserName}")
     @PUT
-
-    public void updateCustomer(@FormParam("inputUserName") String inputUserName, @FormParam("inputEmail") String inputEmail) throws SQLException {
+    @Consumes({"application/text"})
+    public void updateCustomer(@PathParam("inputUserName") String inputUserName, String inputEmail) throws SQLException {
         System.out.println(inputUserName + inputEmail);
         Connection myConn = null;
         Statement myStmt = null;
